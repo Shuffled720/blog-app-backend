1cee022120f4531b00fdeb640d84b7256fb09e72
vishal <vishal@outlook.com>
2022-11-17 01:32:01 +0530

    sec

--- database/db.js
+++ database/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
+
+
 const Connection = async (username, password) => {
-    const URL = `mongodb://localhost:27017/mernblog`;
+    const URL=`mongodb+srv://${username}:${password}@cluster0.yz4uzpn.mongodb.net/?retryWrites=true&w=majority`;
+    // const URL = `mongodb://${username}:${password}@blogweb-shard-00-00.ch1hk.mongodb.net:27017,blogweb-shard-00-01.ch1hk.mongodb.net:27017,blogweb-shard-00-02.ch1hk.mongodb.net:27017/BLOG?ssl=true&replicaSet=atlas-lhtsci-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
         console.log('Database connected successfully');
